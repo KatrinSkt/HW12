11775723409af93427a5e0393df4468b8045a182
Katrin_skt <katrinpivzavod@mail.ru>
2024-03-01 22:43:53 +0300

    Сделала ДЗ 12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Author bookOne = new Author("Михаил", "Лермонтов");
+        Book one = new Book("Герой нашего времени", bookOne, 2022);
+        Author bookTwo = new Author("Оскар", "Уайльд");
+        Book two = new Book("Портрет Дориана Грея", bookTwo, 2018);
+        System.out.println("Название книги: " + one.getBookName() + ", автор: " + bookOne.getName() + " " + bookOne.getSurname()+ ", год издания " + one.getPublicationYear());
+        System.out.println();
+        System.out.println("Название книги: " + two.getBookName() + ", автор: " + bookTwo.getName() + " " + bookTwo.getSurname()+ ", год издания " + two.getPublicationYear());
+        two.setPublicationYear(2022);
+        System.out.println("Новый год издания " + two.getPublicationYear());
     }
 }
